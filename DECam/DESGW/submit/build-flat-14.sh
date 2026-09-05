9c177e984437f112ba8a8baf9793a1e7abc4f8b4
Sean MacBride <sean.p.macbride@gmail.com>
2026-09-05 12:05:56 +0200

    Adding flat building scripts

---
+++ DECam/DESGW/submit/build-flat-14.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/bash -l
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=30
+#SBATCH --mem=116GB
+#SBATCH --time=48:00:00
+#SBATCH -o /shares/soares-santos.physik.uzh/fitsFiles/calibs/logs/cpFlat_14_%j.out
+#SBATCH -e /shares/soares-santos.physik.uzh/fitsFiles/calibs/logs/cpFlat_14_%j.err
+
+source /shares/soares-santos.physik.uzh/repos/Butler-imports/s3it_setup/DESGW_CONFIGS
+module load miniforge3
+source /shares/soares-santos.physik.uzh/envs/lsst_stack/loadLSST.sh
+setup lsst_distrib -c
+
+LOGFILE=$LOGDIR/cpFlat_14.log
+
+date | tee $LOGFILE
+pipetask --long-log run --register-dataset-types -j 30 \
+-b $REPO --instrument lsst.obs.decam.DarkEnergyCamera \
+-i DECam/raw/all,DECam/calib/curated/19700101T000000Z,DECam/calib/unbounded,DECam/calib/bias,DECam/calib/template/overscanRaw \
+-o DECam/calib/template/flat/14 \
+-p $CP_PIPE_DIR/pipelines/DECam/cpFlat.yaml \
+-d "instrument='DECam' AND exposure.observation_type='dome flat' AND exposure.day_obs >= 20140000 AND exposure.day_obs <= 20149999" \
+2>&1 | tee -a $LOGFILE
+date | tee -a $LOGFILE
