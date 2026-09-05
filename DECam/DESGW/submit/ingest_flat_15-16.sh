fd8a2782865e9d2ba6bf8e3be79a135593124693
Sean MacBride <sean.p.macbride@gmail.com>
2026-09-05 12:06:14 +0200

    Adding flat ingestion shell scripts

---
+++ DECam/DESGW/submit/ingest_flat_15-16.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash -l
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=30
+#SBATCH --mem=116GB
+#SBATCH --time=48:00:00
+#SBATCH -o /shares/soares-santos.physik.uzh/fitsFiles/calibs/logs/ingest_flat_15-16_%j.out
+#SBATCH -e /shares/soares-santos.physik.uzh/fitsFiles/calibs/logs/ingest_flat_15-16_%j.err
+
+source /shares/soares-santos.physik.uzh/repos/Butler-imports/s3it_setup/DESGW_CONFIGS
+module load miniforge3
+source /shares/soares-santos.physik.uzh/envs/lsst_stack/loadLSST.sh
+setup lsst_distrib -c
+
+FLATFILES=/shares/soares-santos.physik.uzh/fitsFiles/calibs/flat/dome_flat_15-16/*.fits.fz
+LOGFILE=$LOGDIR/ingest_flat_15-16.log
+
+date | tee $LOGFILE
+butler ingest-raws $REPO $FLATFILES --transfer link \
+2>&1 | tee -a $LOGFILE
+date | tee -a $LOGFILE
